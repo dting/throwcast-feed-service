3b371396e21746379e7691ec6c4b15cad13a67af
Dennis Ting <dennisting@gmail.com>
2016-09-22 01:33:06 -0700

    Add more podcasts

--- seed/index.js
+++ seed/index.js
@@ -5,21 +5,28 @@ const utils = require('../utils');
 const { Playlist, Podcast, Station } = require('../db');
 
 const feeds = [
-  'http://feeds.themoth.org/themothpodcast',
-  'http://feeds.serialpodcast.org/serialpodcast',
-  'http://www.npr.org/templates/rss/podlayer.php?id=1014',
-  'http://www.bloomberg.com/feeds/podcasts/masters_in_business.xml',
-  'http://podcasts.cstv.com/feeds/fantasyfootball.xml',
-  'http://podcasts.cstv.com/feeds/nba.xml',
-  'http://feeds.99percentinvisible.org/99percentinvisible',
-  'https://fivejs.codeschool.com/feed.rss',
-  'https://feeds.feedwrench.com/AdventuresInAngular.rss',
-  'http://feeds.feedburner.com/boagworldpodcast',
-  'http://shoptalkshow.com/feed/podcast',
-  'https://feeds.feedwrench.com/JavaScriptJabber.rss',
-  'http://feeds.feedburner.com/NodeUp',
-  'http://feeds.5by5.tv/webahead',
-  'http://feeds.feedburner.com/dancarlin/history',
+  'http://feeds.themoth.org/themothpodcast', // The Moth Podcast
+  'http://feeds.serialpodcast.org/serialpodcast', // Serial
+  'http://www.npr.org/templates/rss/podlayer.php?id=1014', // Politics : NPR
+  'http://www.npr.org/rss/podcast.php?id=344098539', // Wait Wait... Don't Tell Me!
+  'http://www.npr.org/rss/podcast.php?id=510313', // How I Built This
+  'http://www.npr.org/rss/podcast.php?id=510307', // Invisibilia
+  'http://www.npr.org/rss/podcast.php?id=381444908', // Fresh Air
+  'http://www.bloomberg.com/feeds/podcasts/masters_in_business.xml', // Masters in Business
+  'http://podcasts.cstv.com/feeds/fantasyfootball.xml', // Fantasy Football Today Podcast
+  'http://podcasts.cstv.com/feeds/nba.xml', // CBS Sports Eye On Basketball Podcast
+  'http://feeds.99percentinvisible.org/99percentinvisible', // 99% Invisible
+  'http://feeds.feedburner.com/RevisionistHistory', // Revisionist History
+  'http://feeds.feedburner.com/freakonomicsradio', // Freakonomics Radio
+  'https://fivejs.codeschool.com/feed.rss', // 5 Minutes of JavaScript
+  'https://feeds.feedwrench.com/AdventuresInAngular.rss', // Adventures in Angular
+  'http://feeds.feedburner.com/boagworldpodcast', // The Boagworld UX Show
+  'http://shoptalkshow.com/feed/podcast', // ShopTalk
+  'https://feeds.feedwrench.com/JavaScriptJabber.rss', // JavaScript Jabber
+  'http://feeds.feedburner.com/NodeUp', // NodeUp
+  'http://feeds.5by5.tv/webahead', // The Web Ahead
+  'http://feeds.feedburner.com/dancarlin/history', // Dan Carlin's Hardcore History
+  'http://feeds.soundcloud.com/users/soundcloud:users:38128127/sounds.rss', // StarTalk Radio
 ];
 
 const opts = {
